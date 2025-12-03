2c1607bb8d50509d24657c529990a49127e95e0d
DanielG815 <daniel.garcia@unas.edu.pe>
2025-12-02 21:06:40 -0500

    conection

--- proyecto-notas/app/src/db/connection.js
+++ proyecto-notas/app/src/db/connection.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}` +
+  `@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  },
 });
 
 module.exports = pool;
+
