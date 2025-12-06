f55125c8bab821b0e051103ffb5436312ef3e0b3
DanielG815 <daniel.garcia@unas.edu.pe>
2025-12-06 17:52:18 -0500

    Reaciendo

--- proyecto-notas/app/src/routes/notesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/notesController');
-const router = express.Router();
-
-router.get('/notes', controller.getNotes);
-router.post('/notes', controller.postNote);
-
-module.exports = router;
--- proyecto-notas/infra/do/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "droplet_ip" {
-  description = "Dirección IPv4 pública del droplet"
-  value       = digitalocean_droplet.app.ipv4_address
-}
