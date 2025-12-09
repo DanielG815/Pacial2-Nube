08ac60052d076e681447cdfa517e7c8c6dec162f
DanielG815 <daniel.garcia@unas.edu.pe>
2025-12-08 21:05:05 -0500

    aplicacion desplegada

--- mi-proyecto-cloud/app/app.py
+++ mi-proyecto-cloud/app/app.py
@@ -1,14 +1,12 @@
 import os
-import time
 import psycopg2
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Configuración de conexión a la Base de Datos
 def get_db_connection():
     conn = psycopg2.connect(
-        host=os.environ.get('DB_HOST', 'localhost'),
+        host=os.environ.get('DB_HOST', 'postgres-service'),
         database=os.environ.get('DB_NAME', 'postgres'),
         user=os.environ.get('DB_USER', 'postgres'),
         password=os.environ.get('DB_PASSWORD', 'password')
@@ -17,20 +15,24 @@ def get_db_connection():
 
 @app.route('/')
 def hello():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    # Crear tabla si no existe
-    cur.execute('CREATE TABLE IF NOT EXISTS visitas (id SERIAL PRIMARY KEY, count INT);')
-    cur.execute('INSERT INTO visitas (count) VALUES (1);')
-    conn.commit()
-    
-    # Contar visitas
-    cur.execute('SELECT count(*) FROM visitas;')
-    total = cur.fetchone()[0]
-    
-    cur.close()
-    conn.close()
-    return f"¡Hola desde Kubernetes! Esta página ha sido visitada {total} veces."
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('CREATE TABLE IF NOT EXISTS visitas (id SERIAL PRIMARY KEY, count INT);')
+        cur.execute('INSERT INTO visitas (count) VALUES (1);')
+        conn.commit()
+        
+        cur.execute('SELECT count(*) FROM visitas;')
+        total = cur.fetchone()[0]
+        
+        cur.close()
+        conn.close()
+        
+        # AQUÍ ESTÁ EL CAMBIO: Usamos la plantilla HTML
+        return render_template('index.html', count=total)
+        
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
